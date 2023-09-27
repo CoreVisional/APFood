700e6a22e9aacfe2ce6fe8716826360911f1c1d6
Alex Chiew <concussion239@gmail.com>
2023-09-27 14:47:05 +0800

    Initial commit

---
+++ src/main/java/com/apu/apfood/APFood.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.apu.apfood;
+
+/**
+ *
+ * @author Alex
+ */
+public class APFood {
+
+    public static void main(String[] args) {
+        // entry point
+    }
+}
