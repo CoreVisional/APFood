71721f3ce64ed3c1a727dbeaf1462a73484f3721
Alex Chiew <concussion239@gmail.com>
2023-09-28 17:45:43 +0800

    Add ImageHelper class to set frame icons from specified file paths

---
+++ src/main/java/com/apu/apfood/helpers/ImageHelper.java
@@ -0,0 +1,15 @@
+package com.apu.apfood.helpers;
+
+import java.awt.Image;
+import java.net.URL;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class ImageHelper {
+        public void setFrameIcon(JFrame frame, String filepath) {
+        URL resource = getClass().getResource(filepath);
+        ImageIcon icon = new ImageIcon(resource);
+        Image image = icon.getImage();
+        frame.setIconImage(image);
+    }
+}
