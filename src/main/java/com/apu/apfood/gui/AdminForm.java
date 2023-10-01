07c4fba23d2160a44c449a927cea699245a2e84f
kit-bryan <study.kitbryan@gmail.com>
2023-10-01 18:48:51 +0800

    Changed file name from Administrator to Admin

--- src/main/java/com/apu/apfood/gui/AdminForm.java
+++ src/main/java/com/apu/apfood/gui/AdminForm.java
@@ -2,7 +2,7 @@ package com.apu.apfood.gui;
 
 import com.apu.apfood.helpers.ImageHelper;
 
-public class AdministratorForm extends javax.swing.JFrame {
+public class AdminForm extends javax.swing.JFrame {
 
     // Instantiate helpers classes
     ImageHelper imageHelper = new ImageHelper();
@@ -10,7 +10,7 @@ public class AdministratorForm extends javax.swing.JFrame {
     /**
      * Creates new form VendorFrame
      */
-    public AdministratorForm() {
+    public AdminForm() {
         initComponents();
         initCustomComponents();
     }
@@ -212,14 +212,18 @@ public class AdministratorForm extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AdministratorForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AdministratorForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AdministratorForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AdministratorForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
@@ -228,7 +232,7 @@ public class AdministratorForm extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new AdministratorForm().setVisible(true);
+                new AdminForm().setVisible(true);
             }
         });
     }
