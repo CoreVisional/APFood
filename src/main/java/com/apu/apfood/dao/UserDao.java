dbbf683d5ae678b3df87e24195807a07c191225a
Alex Chiew <concussion239@gmail.com>
2023-10-06 14:09:27 +0800

    Add DAO folder with generic APFoodDao class with add and update methods and UserDao with an override update method

---
+++ src/main/java/com/apu/apfood/dao/UserDao.java
@@ -0,0 +1,15 @@
+package com.apu.apfood.dao;
+
+import com.apu.apfood.models.User;
+
+/**
+ *
+ * @author Alex
+ */
+public class UserDao extends APFoodDao<User> {
+    
+    @Override
+    public void update(User user) {
+        
+    }
+}
