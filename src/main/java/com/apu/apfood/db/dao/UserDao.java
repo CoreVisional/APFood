5c90d44d04ab06618ab5536ceeee85a9ba08cd4f
Alex Chiew <concussion239@gmail.com>
2023-10-10 21:25:38 +0800

    Extend UserDao to add serialization logic and specify file path and headers for user data storage

---
+++ src/main/java/com/apu/apfood/db/dao/UserDao.java
@@ -0,0 +1,27 @@
+package com.apu.apfood.db.dao;
+
+import com.apu.apfood.db.models.User;
+
+/**
+ *
+ * @author Alex
+ */
+public class UserDao extends APFoodDao<User> {
+
+    private static final String USER_FILEPATH = "/src/main/java/com/apu/apfood/db/datafiles/users.txt";
+    private static final String HEADERS = "id| name| email| password| role\n";
+    
+    public UserDao() {
+        super(USER_FILEPATH, HEADERS);
+    }
+    
+    @Override
+    protected String serialize(User user) {
+        return user.getName() + "| " + user.getEmail() + "| " + new String(user.getPassword()) + user.getRole() + "\n"; 
+    }
+    
+    @Override
+    public void update(User user) {
+        
+    }
+}
