ce71aed2e9c85a8f3678c78f6b079edf1f68fc47
Alex Chiew <concussion239@gmail.com>
2023-10-30 11:13:59 +0800

    Add button panel switcher to switch card for card layout in GUIHelper

--- src/main/java/com/apu/apfood/helpers/GUIHelper.java
+++ src/main/java/com/apu/apfood/helpers/GUIHelper.java
@@ -1,15 +1,26 @@
 package com.apu.apfood.helpers;
 
+import java.awt.CardLayout;
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GUIHelper {
 
-        // Center, Display and Focus Jframe
-        public static void JFrameSetup(JFrame jframe) {
-            // Center the JFrame
-            jframe.setLocationRelativeTo(null);
-            jframe.setVisible(true);
-            // Auto focus on Jframe content pane
-            jframe.getContentPane().requestFocusInWindow();
-        }
+    // Center, Display and Focus Jframe
+    public static void JFrameSetup(JFrame jframe) {
+        // Center the JFrame
+        jframe.setLocationRelativeTo(null);
+        jframe.setVisible(true);
+        // Auto focus on Jframe content pane
+        jframe.getContentPane().requestFocusInWindow();
+    }
+        
+    public void buttonPanelSwitcher(JButton button, JPanel parentPanel, String targetPanel) {
+        button.addActionListener((ActionEvent e) -> {
+            CardLayout card = (CardLayout)parentPanel.getLayout();
+            card.show(parentPanel, targetPanel);
+        });
+    }
 }
