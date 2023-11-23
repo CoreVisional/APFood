98e9d2fcef18ec241a71fdfd9f8842b3917dd8b5
Alex Chiew <concussion239@gmail.com>
2023-11-23 14:41:36 +0800

    Update TableHelper to add generic populateTable and createTableMap methods, and support for Double type cell alignment

--- src/main/java/com/apu/apfood/helpers/TableHelper.java
+++ src/main/java/com/apu/apfood/helpers/TableHelper.java
@@ -1,6 +1,9 @@
 package com.apu.apfood.helpers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
@@ -18,20 +21,65 @@ public class TableHelper {
         centerRenderer.setHorizontalAlignment( JLabel.CENTER );
         tableName.setDefaultRenderer(String.class, centerRenderer);
         tableName.setDefaultRenderer(Integer.class, centerRenderer);
+        tableName.setDefaultRenderer(Double.class, centerRenderer);
         tableName.setDefaultRenderer(Object.class, centerRenderer);
     }
     
-    public void populateTable(List<String[]> data, JTable tableName) {
-        DefaultTableModel model = (DefaultTableModel) tableName.getModel();
+    // Generic method with index control
+    public <T> void populateTable(List<T> data, JTable table, Function<T, Object[]> rowMapper, boolean hasIndex) {
+        DefaultTableModel model = (DefaultTableModel) table.getModel();
+        
         model.setRowCount(0);
 
         int index = 1;
-
-        for (String[] row : data) {
-            String[] rowDataWithIndex = new String[row.length + 1];
-            rowDataWithIndex[0] = String.valueOf(index++);
-            System.arraycopy(row, 0, rowDataWithIndex, 1, row.length);
+        
+        for (T item : data) {
+            Object[] rowData = rowMapper.apply(item);
+            Object[] rowDataWithIndex;
+            if (hasIndex) {
+                rowDataWithIndex = new Object[rowData.length + 1];
+                rowDataWithIndex[0] = index++;
+                System.arraycopy(rowData, 0, rowDataWithIndex, 1, rowData.length);
+            } else {
+                rowDataWithIndex = rowData;
+            }
             model.addRow(rowDataWithIndex);
         }
+    }
+    
+    // Overloaded generic method assuming no index
+    public <T> void populateTable(List<T> data, JTable table, Function<T, Object[]> rowMapper) {
+        populateTable(data, table, rowMapper, false);
+    }
+    
+    /**
+     * Generic method to create a map from a JTable.
+     * 
+     * @param <K> the type of keys maintained by the map
+     * @param <V> the type of mapped values
+     * @param table the JTable to extract data from
+     * @param keyColumnIndex the column index for keys
+     * @param valueColumnIndex the column index for values
+     * @param keyMapper function to map table value to key type
+     * @param valueMapper function to map table value to value type
+     * @return a map containing keys and values extracted from the table
+     */
+    public <K, V> Map<K, V> createTableMap(JTable table, 
+                                               int keyColumnIndex, 
+                                               int valueColumnIndex, 
+                                               Function<Object, K> keyMapper, 
+                                               Function<Object, V> valueMapper) {
+        
+        Map<K, V> map = new HashMap<>();
+        
+        for (int i = 0; i < table.getRowCount(); i++) {
+            K key = keyMapper.apply(table.getValueAt(i, keyColumnIndex));
+            
+            V value = valueMapper.apply(table.getValueAt(i, valueColumnIndex));
+            
+            map.put(key, value);
+        }
+        
+        return map;
     }
 }
