04fe4739e0bbc376971882be4c94579cec477304
Alex Chiew <concussion239@gmail.com>
2023-12-08 20:11:15 +0800

    Implement getAllAdmins and getNextAdmin methods for admin user management in UserService

--- src/main/java/com/apu/apfood/services/UserService.java
+++ src/main/java/com/apu/apfood/services/UserService.java
@@ -2,6 +2,8 @@ package com.apu.apfood.services;
 
 import com.apu.apfood.db.dao.UserDao;
 import com.apu.apfood.db.models.User;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -10,6 +12,7 @@ import com.apu.apfood.db.models.User;
 public class UserService {
 
     private final UserDao userDao;
+    private static int lastAssignedAdminId = 0;
 
     public UserService(UserDao userDao) {
         this.userDao = userDao;
@@ -27,5 +30,32 @@ public class UserService {
     public boolean isCustomer(int userId) {
         User user = userDao.getUserById(userId);
         return user != null && "customer".equalsIgnoreCase(user.getRole());
+    }
+    
+    public List<User> getAllAdmins() {
+        return userDao.getAllUsers().stream()
+                      .filter(user -> "admin".equalsIgnoreCase(user.getRole()))
+                      .collect(Collectors.toList());
+    }
+    
+    public User getNextAdmin() {
+        List<User> admins = getAllAdmins();
+        if (admins.isEmpty()) {
+            return null;
+        }
+
+        // Round-robin assignment
+        int nextAdminIndex = 0;
+        if (lastAssignedAdminId != 0) {
+            for (int i = 0; i < admins.size(); i++) {
+                if (admins.get(i).getId() == lastAssignedAdminId) {
+                    nextAdminIndex = (i + 1) % admins.size();
+                    break;
+                }
+            }
+        }
+
+        lastAssignedAdminId = admins.get(nextAdminIndex).getId();
+        return admins.get(nextAdminIndex);
     }
 }
